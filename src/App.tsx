c9499bf6dea0e6e4a046211671e0be23ff37e1c5
Jawad Rafique <jawadrafique07@gmail.com>
2021-09-24 23:22:39 +0500

    State Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import "./App.css";
+import { actionCreators, State } from "./state";
+
+interface IState {
+    deposit: number;
+    withdraw: number;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
+        actionCreators,
+        dispatch
+    );
+    const amount = useSelector((state: State) => state.bank);
+
+    const [inputAmount, setInputAmount] = useState<IState>({
+        deposit: 0,
+        withdraw: 0,
+    });
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputAmount({
+            ...inputAmount,
+            [e.target.name]: e.target.value,
+        });
+    };
+    return (
+        <div>
+            <h3>{amount}</h3>
+            <input
+                type="number"
+                name="deposit"
+                placeholder="0"
+                value={inputAmount.deposit}
+                onChange={handleInputChange}
+            />
+            <button
+                onClick={() => {
+                    depositMoney(Math.floor(inputAmount.deposit));
+                    inputAmount.deposit = 0;
+                }}
+            >
+                Deposit
+            </button>
+            <br></br>
+            <input
+                type="number"
+                name="withdraw"
+                value={inputAmount.withdraw}
+                onChange={handleInputChange}
+            />
+            <button
+                onClick={() => {
+                    withdrawMoney(Math.floor(inputAmount.withdraw));
+                    inputAmount.withdraw = 0;
+                }}
+            >
+                Withdraw
+            </button>
+            <br></br>
+            <button onClick={() => bankrupt()}>Bankrupt</button>
+        </div>
+    );
 }
 
 export default App;
